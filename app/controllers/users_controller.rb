a9a004c608236ae1a9c8e6d967adf942a2187883
Dave Woodall <dmwoodall@gmail.com>
2012-06-10 10:42:03 -0500

    added a user sign up;

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+	def new		
+		@user = User.new
+	end
+	
+	def create
+		@user = User.create(params[:user])
+		if @user.save
+			flash[:notice] = "success!"
+			redirect_to root_url
+		else
+			flash[:notice] = "Sorry!"
+			redirect_to new_user_url
+		end
+	end
+	
+end
